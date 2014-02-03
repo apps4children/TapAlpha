2c4cd7dcceb6bba1f38508ccbd4ad0d1c63bd935
kaspo <apps4children@gmail.com>
2014-02-03 20:26:32 +0530

    Initial Commit

---
+++ TapAlpha/src/com/iitk/Settings.java
@@ -0,0 +1,27 @@
+package com.iitk;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity
+{
+@Override
+protected void onCreate(Bundle savedInstanceState) 
+{
+   super.onCreate(savedInstanceState);
+   addPreferencesFromResource(R.xml.settings);
+   Preference button = (Preference)getPreferenceManager().findPreference("exitlink");      
+   if(button!= null) 
+   {
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() 
+        {
+
+        	public boolean onPreferenceClick(Preference arg0)
+        	{
+                        finish();   
+                        return true;
+             }
+        });     
+   }
+}
+}
